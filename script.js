5ef36fcb7fc1aed726ff0266a3c61c0b9954e4e2
ShyroVO <bosquet.laurie@outlook.com>
2021-01-12 15:17:44 +0100

    0.5 /

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+
+// Play / Pause:
+let playButton = document.getElementsByClassName('player__button');
+
+$('.player__button').on('click', function (){
+    if (playButton[0].innerHTML === "||"){
+        $('.player__video')[0].pause();
+        playButton[0].innerHTML = "►";
+    }
+    else {
+        $('.player__video')[0].play();
+        playButton[0].innerHTML = "||";
+    }
+})
+
+//timeupdate:
+
